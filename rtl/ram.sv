f81b42632856368897f3d0a2cc5bb7b43dc5431b
German Pinedo <germanpinedo13@gmail.com>
2023-06-28 21:58:27 +0000

    [VERIF] Updated port1_agent, transaction (txn)

--- rtl/ram.sv
+++ rtl/ram.sv
@@ -8,11 +8,11 @@ module(
     input [31:0] din0,
     output [31:0] dout0,
 
-    // Port 0, R-only
+    // Port 1, R-only
     input clk1,
-    input cs0,
-    input [9:0] addr0,
-    output [31:0] dout0
+    input cs1,
+    input [9:0] addr1,
+    output [31:0] dout1
 );
 
 endmodule
--- verif/tb/tb.sv
+++ verif/tb/tb.sv
@@ -1,9 +1,37 @@
+`include "uvm_macros.svh"
+`include "ram_base_test.svh"
+// pkg for cadence
 module tb();
 
-    test test_; // HANDLER for test
+
+
+    logic clk0, clk1;
+
+    port1_intf port1_if(clk1);
+
+    ram DUT(
+        // Port 0, R/W
+        clk0,
+        cs0,
+        we0,
+        wmask,
+        addr0,
+        din0,
+        dout0,
+
+        // Port 1, R-only
+        port1_if.clk1,
+        port1_if.cs1,
+        port1_if.addr1,
+        port1_if.dout1
+    );
+
+    
+    initial begin
+        uvm_config_db #(virtual port1_intf)::set(null, "*", "port1_vif", port1_if);
+    end
 
     initial begin
-        test_ = new("test_", null);
         run_test();
     end
 
