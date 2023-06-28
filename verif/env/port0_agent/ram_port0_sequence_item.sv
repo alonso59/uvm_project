7e34d07312fa91096e81aa01a9779155ce6cbf01
Greciamontoyaz <67496742+Greciamontoyaz@users.noreply.github.com>
2023-06-27 21:59:30 -0600

    Update ram_port0_sequence_item.sv

--- verif/env/port0_agent/ram_port0_sequence_item.sv
+++ verif/env/port0_agent/ram_port0_sequence_item.sv
@@ -1,29 +1,31 @@
-class ram_port0_sequencer extends uvm_sequence_item;
+class ram_port0_sequence_item extends uvm_sequence_item;
   
   //data and control fields
   
   rand bit cs0;// The memory is selected when it is set at low level. 
-  rand bit wr_en;
-  rand bit [3:0] wmask;
+  rand bit we0;
+  rand bit [3:0] wmask0;
   rand bit [`ADDR_WIDTH-1:0] addr0;
-  rand bit [`DATA_WIDTH-1:0] din0;
+  rand bit [`DATA_WIDTH-1:0] din1;
   rand bit [`DATA_WIDTH-1:0] dout0;
   
 
 
   
   //Utility and Field macros,
-  `uvm_object_utils_begin(ram_port0_sequencer)
-    `uvm_field_int(addr,UVM_ALL_ON)
-    `uvm_field_int(wr_en,UVM_ALL_ON)
-    `uvm_field_int(rd_en,UVM_ALL_ON)
-    `uvm_field_int(wdata,UVM_ALL_ON)
+  `uvm_object_utils_begin(ram_port0_sequence_item)
+    `uvm_field_int(cs0,UVM_ALL_ON)
+    `uvm_field_int(we0,UVM_ALL_ON)
+    `uvm_field_int(wmask0,UVM_ALL_ON)
+    `uvm_field_int(din1,UVM_ALL_ON)
+    `uvm_field_int(dout0,UVM_ALL_ON)
+
   `uvm_object_utils_end
   
   //Constructor
-  function new(string name = "ram_port0_sequencer");
+  function new(string name = "ram_port0_sequence_item");
     super.new(name);
   endfunction
   
   
-endclass:ram_port0_sequencer
+endclass:ram_port0_sequence_item
