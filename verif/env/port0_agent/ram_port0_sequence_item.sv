a48f9af31cd20f66003af5e9390fe69af64a839c
Greciamontoyaz <67496742+Greciamontoyaz@users.noreply.github.com>
2023-06-28 18:10:48 -0600

    Update ram_port0_sequence_item.sv

--- verif/env/port0_agent/ram_port0_sequence_item.sv
+++ verif/env/port0_agent/ram_port0_sequence_item.sv
@@ -6,8 +6,8 @@ class ram_port0_sequence_item extends uvm_sequence_item;
   rand bit we0;
   rand bit [3:0] wmask0;
   rand bit [`ADDR_WIDTH-1:0] addr0;
-  rand bit [`DATA_WIDTH-1:0] din1;
-  rand bit [`DATA_WIDTH-1:0] dout0;
+  rand logic [`DATA_WIDTH-1:0] din0;
+  //rand bit [`DATA_WIDTH-1:0] dout1;
   
 
 
@@ -18,7 +18,7 @@ class ram_port0_sequence_item extends uvm_sequence_item;
     `uvm_field_int(we0,UVM_ALL_ON)
     `uvm_field_int(wmask0,UVM_ALL_ON)
     `uvm_field_int(din1,UVM_ALL_ON)
-    `uvm_field_int(dout0,UVM_ALL_ON)
+    //`uvm_field_int(dout0,UVM_ALL_ON)
 
   `uvm_object_utils_end
   
