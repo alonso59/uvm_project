12c87ba467c3894d81497244c9adea932e491b19
Emanuel Murillo <emanuel.muga13@gmail.com>
2023-06-30 12:40:10 -0600

    [PORT0]: se hacen cambios al monitor

--- verif/env/port0_agent/ram_port0_monitor.svh
+++ verif/env/port0_agent/ram_port0_monitor.svh
@@ -13,25 +13,46 @@ class ram_port0_monitor extends uvm_monitor;
   //bit checks_enable = 1;       // Control checking in monitor and interface.
   //bit coverage_enable = 1;     // Control coverage in monitor and interface.
 
-  uvm_analisys_port #(ram_port0_txn) item_collected_port;
+
+  uvm_analysis_port #(ram_port0_txn) Ana_port;
 
   function new(input string name, uvm_component parent);
     super.new(name,parent);
-    item_collected_port = new("item_collected_port", this); //Analisys port constructor.
+    Ana_port = new("Ana_port", this); //Analisys port constructor.
   endfunction
-  
+
   virtual task run_phase(uvm_phase phase);
     `uvm_info(get_name(), "Run Phase", UVM_MEDIUM)
-    collect_transactions();  //Collector task
+
+    forever begin
+
+      ram_port0_txn txn;
+
+      @(posedge port0_vir.clk0);
+      txn = ram_port0_txn::type_id::create("txn");
+	    txn.we0; = port0_vif.we0;
+	    txn.cs0; = port0_vif.cs0;
+	    txn.wmask0 = port0_vif.wmask0;
+	    txn.addr0 = port0_vif.addr0;
+	    txn.din0 = port0_vif.din0;
+	    txn.dout0 = port0_vif.dout0; 
+
+      Ana_port.write(txn);    // write to analisys port
+    end
   endtask : run
 
-  virtual protected task collect_transactions();
-    forever begin
-      @(posedge port0_vir.clk0);
-        // Collect the data from the DUT into trans_collected/
-        item_collected_port.write(trans_collected);  // Write transacrion via Analisys port.
-    end
-  endtask : collect_transactions
+  // virtual task run_phase(uvm_phase phase);
+  //   `uvm_info(get_name(), "Run Phase", UVM_MEDIUM)
+  //   collect_transactions();  //Collector task
+  // endtask : run
+
+  // virtual protected task collect_transactions();
+  //   forever begin
+  //     @(posedge port0_vir.clk0);
+  //       // Collect the data from the DUT into trans_collected/
+  //       item_collected_port.write(trans_collected);  // Write transacrion via Analisys port.
+  //   end
+  // endtask : collect_transactions
 
 
   
--- verif/intf/ram_port0_inft.sv
+++ verif/intf/ram_port0_inft.sv
@@ -11,6 +11,6 @@ interface port1_intf(input logic clk0);
 	logic we0;
 	logic [`DATA_WIDTH/4-1:0] wmask0;
 	logic [`ADDR_WIDTH-1:0] addr0;
-	logic [`DATA_WIDTH-1:0] din1;
+	logic [`DATA_WIDTH-1:0] din0;
 	logic [`DATA_WIDTH-1:0] dout0;
 endinterface
