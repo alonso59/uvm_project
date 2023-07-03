468da8f576629130a89a5af1d46dc34737eb9b9a
German Pinedo <germanpinedo13@gmail.com>
2023-07-03 19:35:34 +0000

    [VERIF] Xcelium packages and compilation ready

--- work/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
+++
@@ -1,8 +0,0 @@
-1688406180 /home/joc/Documents/german/uvm_project/verif/intf/ram_port1_intf.sv
-1688408711 /home/joc/Documents/german/uvm_project/verif/env/port1_agent/ram_port1_agent_pkg.svh
-1688409479 /home/joc/Documents/german/uvm_project/definitions/definitions.sv
-1688410398 /home/joc/Documents/german/uvm_project/verif/env/ram_env_pkg.svh
-1688411373 /home/joc/Documents/german/uvm_project/verif/tb/tb.sv
-1688408317 /home/joc/Documents/german/uvm_project/verif/env/ram_env_pkg.sv
-1688410008 /home/joc/Documents/german/uvm_project/rtl/ram.sv
-1688409738 /home/joc/Documents/german/uvm_project/verif/intf/ram_port0_intf.sv
--- work/xcelium.d/run.lnx8664.20.09.d/files.ts
+++
@@ -1,6 +0,0 @@
-1688409479 /home/joc/Documents/german/uvm_project/definitions/definitions.sv
-1688410008 /home/joc/Documents/german/uvm_project/rtl/ram.sv
-1688406180 /home/joc/Documents/german/uvm_project/verif/intf/ram_port1_intf.sv
-1688411373 /home/joc/Documents/german/uvm_project/verif/tb/tb.sv
-1688410398 /home/joc/Documents/german/uvm_project/verif/env/ram_env_pkg.svh
-1688409738 /home/joc/Documents/german/uvm_project/verif/intf/ram_port0_intf.sv
