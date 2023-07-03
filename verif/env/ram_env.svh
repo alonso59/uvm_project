caeff7e5aba9bd0f79471fab91fcb1bfc490cdf9
German Pinedo <germanpinedo13@gmail.com>
2023-07-03 19:39:12 +0000

    [VERIF] Created ram_env.svh and coreated port1_agent

--- verif/env/ram_env.svh
+++ verif/env/ram_env.svh
@@ -1,7 +1,14 @@
+/*
+=============================================================
+    Owners      : Everyone (Grecia, Manuel, Emmanuel, German)
+    Last update    : 03 Jul 2023 by German
+=============================================================            
+*/
+
 class ram_env extends uvm_env;
     `uvm_component_utils(ram_env)
     
-    agent agent_;	// HANDLER for agent
+  ram_port1_agent port1_agent;	// HANDLER for agent
     
     function new (input string name, uvm_component parent);
       super.new(name,parent);
@@ -10,7 +17,7 @@ class ram_env extends uvm_env;
     virtual function void build_phase(uvm_phase phase);
       super.build_phase(phase);
       `uvm_info(get_name(), "Build Phase", UVM_NONE)
-      agent_ = agent::type_id::create("agent_",this); // // BUILD using factory object of agent
+      port1_agent = ram_port1_agent::type_id::create("port1_agent",this); // // BUILD using factory object of agent
     endfunction
     
     virtual function void connect_phase(uvm_phase phase);
