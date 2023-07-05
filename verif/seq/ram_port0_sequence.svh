ba1fe99046c06c3ca1c552afb4b676130dc90607
Greciamontoyaz <67496742+Greciamontoyaz@users.noreply.github.com>
2023-07-05 16:41:13 -0600

    Add files via upload

---
+++ verif/seq/ram_port0_sequence.svh
@@ -0,0 +1,25 @@
+//Sequence generates the stimulus and sends to driver via sequencer
+
+class ram_port0_sequence extends uvm_sequence;
+  
+  `uvm_sequence_utils(ram_port0_sequence,ram_port0_sequencer)
+  
+  //Constructor
+  function new(string name = "ram_port0_sequence");
+    super.new(name);
+    
+  endfunction
+
+
+    virtual task body();
+
+    req = ram_port0_sequence_item::type_id::create("req");
+    wait_for_grant();
+    req.randomize();
+    send_request(req);
+    wait_for_item_done();
+    
+    endtask 
+
+  
+endclass:ram_port0_sequence
---
+++ verif/seq/ram_port0_wr_seq.svh
@@ -0,0 +1,40 @@
+/*
+=============================================================
+    Owners      : Grecia Montoya / Emanuel Murillo
+    Last update    : 5 Jul 2023 by Emanuel Murillo
+=============================================================            
+*/
+
+//WRITE SEQUENCE
+class mem_wr_seq extends uvm_sequence;
+  
+  `uvm_object_utils(mem_wr_seq)
+   
+  //Constructor
+  function new(string name = "mem_wr_seq");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    `uvm_do_with(req,{req.we0 == 1;})
+    `uvm_do_with(req,{req.cs0 == 0;})
+  endtask
+
+
+  /*
+
+    ram_port0_sequence_item txn;
+
+    task body();
+    txn=ram_port0_sequence_item::type_id::create("txn");
+    start_item(txn);
+    txn.we0=1;
+    txn.cs=0;
+    finish_item(txn);
+  endtask:body
+
+  
+  
+  */
+  
+endclass
