2f4066cb88009cc91f02a9a6f8dfa786d9d10e38
Manyberto <manuel_51akd@hotmail.com>
2023-07-17 16:40:31 -0600

    [VERIF] name issue

--- verif/env/port0_agent/ram_port0_sequence_item.svh
+++ verif/env/port0_agent/ram_port0_sequence_item.svh
@@ -2,8 +2,8 @@ class ram_port0_sequence_item extends uvm_sequence_item;
   
   //data and control fields
   
-  rand bit cs0;// The memory is selected when it is set at low level. 
-  rand bit we0;
+  rand bit csb0;// The memory is selected when it is set at low level. 
+  rand bit web0;
   rand bit [3:0] wmask0;
   rand bit [`ADDR_WIDTH-1:0] addr0;
   rand logic [`DATA_WIDTH-1:0] din0;
@@ -14,8 +14,8 @@ class ram_port0_sequence_item extends uvm_sequence_item;
   
   //Utility and Field macros,
   `uvm_object_utils_begin(ram_port0_sequence_item)
-    `uvm_field_int(cs0,UVM_ALL_ON)
-    `uvm_field_int(we0,UVM_ALL_ON)
+    `uvm_field_int(csb0,UVM_ALL_ON)
+    `uvm_field_int(web0,UVM_ALL_ON)
     `uvm_field_int(wmask0,UVM_ALL_ON)
    `uvm_field_int(din0,UVM_ALL_ON)
     `uvm_field_int(dout0,UVM_ALL_ON)
--- verif/env/port1_agent/ram_port1_sequence_item.svh
+++ verif/env/port1_agent/ram_port1_sequence_item.svh
@@ -10,7 +10,7 @@
 class ram_port1_sequence_item extends uvm_sequence_item;
   
     // Data and control fields
-    rand logic cs1;
+    rand logic csb1;
     rand logic [`ADDR_WIDTH-1:0] addr1;
     rand logic [`DATA_WIDTH-1:0] dout1;
 
@@ -21,7 +21,7 @@ class ram_port1_sequence_item extends uvm_sequence_item;
     
     // Utility and Field macros,
     `uvm_object_utils_begin(ram_port1_sequence_item)
-    `uvm_field_int(cs1, UVM_HEX)
+    `uvm_field_int(csb1, UVM_HEX)
     `uvm_field_int(addr1, UVM_HEX)
     `uvm_field_int(dout1, UVM_HEX)
     `uvm_object_utils_end
