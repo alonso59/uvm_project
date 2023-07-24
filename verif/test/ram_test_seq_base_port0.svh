07c89e9d196c3da919e8c6b6fb7dc2fb1f81e880
emanuelmuga <87344439+emanuelmuga@users.noreply.github.com>
2023-07-24 13:02:57 -0600

    [VERIF]  base_test_port0: fix uvm_info

--- verif/test/ram_test_seq_base_port0.svh
+++ verif/test/ram_test_seq_base_port0.svh
@@ -30,8 +30,6 @@ class ram_test_seq_base_port0 extends ram_base_test;
                 seq0_rd = ram_port0_rd_seq::type_id::create("seq0_rd");
                 seq0_rd.start(ram_env_i.port0_agent.sqr);
                 #500ns
-                `uvm_info(get_name(),$sformatf("addr0: %0h, "),seq0_wr.addr0,)
-
             end
         #1us;
         phase.drop_objection(this);
