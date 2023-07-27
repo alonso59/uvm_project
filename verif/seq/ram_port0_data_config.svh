adef245ff105f3cd526e5ce0111a955731fa14bc
Greciamontoyaz <67496742+Greciamontoyaz@users.noreply.github.com>
2023-07-27 12:23:03 -0600

    Update ram_port0_data_config.svh

--- verif/seq/ram_port0_data_config.svh
+++ verif/seq/ram_port0_data_config.svh
@@ -1,21 +1,19 @@
 // Define the data configuration object
-class ram_port0_data_config;
-  rand bit csb0;
-  rand bit web0;
-  rand bit [3:0] wmask0;
-  rand bit [ADDR_WIDTH-1:0] addr0;
-  rand logic [DATA_WIDTH-1:0] din0;
+class ram_port0_data_config extends uvm_object ;
+   `uvm_object_utils(ram_port0_data_config)
+   bit csb0;
+   bit web0;
+   bit [3:0] wmask0;
+   bit [ADDR_WIDTH-1:0] addr0;
+   logic [DATA_WIDTH-1:0] din0;
   bit [DATA_WIDTH-1:0] dout0;
 
   // Constructor
-  function new();
-    csb0 = 0;
-    web0 = 0;
-    wmask0 = 4'b0000;
-    addr0 = 0;
-    din0 = '0;
-    dout0 = '0;
-  endfunction
+function new (string name = "ram_port0_data_config");
+
+   super.new(name);
+
+ endfunction: new
 
  
 endclass : ram_port0_data_config
